e3ca8c7e18934c87bade94510e11a390a7752e79
roig <roigrosa88@gmail.com>
2021-04-05 15:07:54 -0400

    passed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+//const currentUser = `Roig`;
+
+//const welcomeMessage = `Welcome to Flatbook, Roig!`;
+
+//const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ROIG!";
+
+//const shortGreeting = `Welcome, R!`;
+
+const currentUser = 'Roig';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
